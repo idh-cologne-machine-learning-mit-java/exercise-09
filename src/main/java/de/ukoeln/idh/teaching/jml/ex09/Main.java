da81363e8056bd3e34611823d351afe3958b68ae
stereolith <mail@lukasmoench.de>
2021-01-12 15:17:07 +0100

    ex09

--- src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
@@ -5,13 +5,31 @@ import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
+import de.ukoeln.idh.teaching.jml.ex09.types.Token;
+import de.ukoeln.idh.teaching.jml.ex09.types.Sentence;
+
 public class Main {
 
 	static JCas jcas;
 
 	public static void main(String[] args) throws ResourceInitializationException, CASException {
 		jcas = JCasFactory.createText("The dog barks.");
-
+		
+		Sentence s = new Sentence(jcas);
+		s.setBegin(0);
+		s.setEnd(jcas.getDocumentText().length() - 1);
+		
+		Token t1 = new Token(jcas);
+		t1.setBegin(0);
+		t1.setEnd(2);
+		
+    Token t2 = new Token(jcas);
+    t2.setBegin(4);
+    t2.setEnd(6);
+    
+    Token t3 = new Token(jcas);
+    t3.setBegin(8);
+    t3.setEnd(12);
 	}
 
 }
