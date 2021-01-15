1e1f940cceb9e22146632d78954919b1e3b55f12
Tholke <oehlke.thomas@gmail.com>
2021-01-15 16:38:20 +0100

    finished exercise 09

--- src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
@@ -5,12 +5,36 @@ import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
+import de.ukoeln.idh.teaching.jml.ex09.types.Token;
+import de.ukoeln.idh.teaching.jml.ex09.types.Sentence;
+
 public class Main {
 
 	static JCas jcas;
 
 	public static void main(String[] args) throws ResourceInitializationException, CASException {
 		jcas = JCasFactory.createText("The dog barks.");
+		
+		Sentence sentence = new Sentence(jcas);
+		sentence.setBegin(0);
+		sentence.setEnd(jcas.getDocumentText().length() - 1);
+		
+		Token token1 = new Token(jcas);
+		token1.setBegin(0);
+		token1.setEnd(2);
+		
+		Token token2 = new Token(jcas);
+		token2.setBegin(4);
+		token2.setEnd(6);
+		
+		Token token3 = new Token(jcas);
+		token3.setBegin(8);
+		token3.setEnd(12);
+		
+		
+		Token token4 = new Token(jcas);
+		token4.setBegin(13);
+		token4.setEnd(13);
 
 	}
 
