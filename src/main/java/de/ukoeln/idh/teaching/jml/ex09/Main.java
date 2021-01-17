8464ee07257ed87110f4ec782dde03cdd9445c46
eliasuk <efaqirza@smail.uni-koeln.de>
2021-01-18 00:51:17 +0100

    Done

--- src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
@@ -1,9 +1,8 @@
-package de.ukoeln.idh.teaching.jml.ex09;
-
-import org.apache.uima.cas.CASException;
-import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
+
+import de.ukoeln.idh.teaching.jml.ex09.types.Token;
+import de.ukoeln.idh.teaching.jml.ex09.types.Sentence;
 
 public class Main {
 
@@ -11,6 +10,10 @@ public class Main {
 
 	public static void main(String[] args) throws ResourceInitializationException, CASException {
 		jcas = JCasFactory.createText("The dog barks.");
+
+		Sentence sentence = new Sentence(jcas);
+		sentence.setBegin(0);
+		sentence.setEnd(jcas.getDocumentText().length() - 1);
 
 	}
 
