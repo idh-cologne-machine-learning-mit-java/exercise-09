54c04ce1e8b6c8899f8eb85ad9230885fa9cd3e5
amikoz <kozenasheva@gmail.com>
2021-01-18 16:25:41 +0100

    done with the exercise

--- src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
@@ -4,6 +4,8 @@ import org.apache.uima.cas.CASException;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
+import de.ukoeln.idh.teaching.jml.ex09.types.Sentence;
+import de.ukoeln.idh.teaching.jml.ex09.types.Token;
 
 public class Main {
 
@@ -12,6 +14,21 @@ public class Main {
 	public static void main(String[] args) throws ResourceInitializationException, CASException {
 		jcas = JCasFactory.createText("The dog barks.");
 
+		Sentence sentence = new Sentence(jcas);
+		sentence.setBegin(0);
+		sentence.setEnd(jcas.getDocumentText().length() - 1);
+
+ 		Token token1 = new Token(jcas);
+		token1.setBegin(sentence.getBegin());
+		token1.setEnd(2);
+
+		Token token2 = new Token(jcas);
+		token2.setBegin(token1.getEnd()+1);
+		token2.setEnd(6);
+
+		Token token3 = new Token(jcas);
+		token3.setBegin(token2.getEnd()+1);
+		token3.setEnd(13);
 	}
 
 }
