50bd06de21fa22abee700e09f4d19225f7e4be41
larac25 <34246312+larac25@users.noreply.github.com>
2021-01-18 22:06:24 +0100

    done (unsolved jcas error)
    couldn't solve error: JCasGen: resourceInitializationExceptionError Exception: Invalid descriptor at file:/Users/Lara/Documents/GitHub/
     exercise-09/src/main/resources/ex09types.xml.

--- src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex09/Main.java
@@ -5,12 +5,19 @@ import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
+import de.ukoeln.idh.teaching.jml.ex09.types.Token;
+import de.ukoeln.idh.teaching.jml.ex09.types.Sentence;
+
 public class Main {
 
 	static JCas jcas;
 
 	public static void main(String[] args) throws ResourceInitializationException, CASException {
 		jcas = JCasFactory.createText("The dog barks.");
+		
+		Sentence sentence = new Sentence(jcas);
+		sentence.setBegin(0);
+		sentence.setEnd(jcas.getDocumentText().length() -1);
 
 	}
 
